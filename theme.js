c427018809fabbd7f43b9cc7a99708eb60ecc250
karine-schobert <schobert.karine@sfr.fr>
2023-06-26 19:52:53 +0200

    javascript changement de theme

---
+++ theme.js
@@ -0,0 +1,117 @@
+const theme ={
+  
+  // ----------------------------------------------------------
+  // Initialisation de notre fonctionnalité thème
+  // ----------------------------------------------------------
+
+  init: function(){
+    
+    theme.addEventChangeButtonGreen();
+    theme.addEventChangeButtonRed();
+    theme.addEventChangeButtonBlue();
+  },
+
+addEventChangeButtonGreen: function (){
+    const buttonGreen = document.querySelector('.theme-button-green');
+    console.log(buttonGreen);
+    buttonGreen.addEventListener('click', theme.handleButtonClickGreen)
+    console.log("mon bouton vert:",buttonGreen );
+
+  },
+
+  addEventChangeButtonRed: function (){
+    // on reccupere les bouton de changment  de theme de couleur 
+
+    const buttonRed = document.querySelector('.theme-button-red');
+    console.log(buttonRed);
+
+    buttonRed.addEventListener('click', theme.handleButtonClickRed)
+    console.log("mon bouton rouge:",buttonRed );
+  },
+  
+  addEventChangeButtonBlue: function (){
+    const buttonBlue = document.querySelector('.theme-button-blue');
+    console.log(buttonBlue);
+      buttonBlue.addEventListener('click', theme.handleButtonClickBlue)
+      console.log("mon bouton blue:",buttonBlue);
+    } ,
+
+
+/*****************************************************************************************************/
+  handleButtonClickGreen :function(eventDetail){
+  console.log(eventDetail.currentTarget);
+  // on récupère le bouton de couleur sur lequel on a cliqué
+  const clickedColorButtonEl = eventDetail.currentTarget;
+  console.log("J'ai cliqué sur le bouton : ", clickedColorButtonEl);
+
+  theme.changeColorThemeGreen(clickedColorButtonEl.id);
+  console.log('changeColorTheme', clickedColorButtonEl.id);
+},
+    
+  handleButtonClickRed :function(event){
+    console.log(event.currentTarget);
+    // on récupère le bouton de couleur sur lequel on a cliqué
+    const clickedColorButtonEl = event.currentTarget;
+    console.log("J'ai cliqué sur le bouton : ", clickedColorButtonEl);
+
+    theme.changeColorThemeRed(clickedColorButtonEl.id);
+    console.log('changeColorTheme', clickedColorButtonEl.id);
+  },
+
+  
+
+  handleButtonClickBlue :function(evt){
+    console.log(evt.currentTarget);
+    // on récupère le bouton de couleur sur lequel on a cliqué
+    const clickedColorButtonEl = evt.currentTarget;
+    console.log("J'ai cliqué sur le bouton : ", clickedColorButtonEl);
+
+    theme.changeColorThemeBlue(clickedColorButtonEl.id);
+    console.log('changeColorTheme', clickedColorButtonEl.id);
+  },
+
+
+/****************************************************************************************************************/
+  changeColorThemeGreen: function (green){
+  console.log();
+  // On recupere l'élement sur lequel on veu ajouter le theme couleur
+  const mainElementGreen =document.querySelector('main');
+  console.log(mainElementGreen);
+
+  mainElementGreen.classList.remove('theme-green');
+  console.log(mainElementGreen);
+
+  mainElementGreen.classList.add('green');
+  console.log(mainElementGreen);
+
+},
+
+  changeColorThemeRed: function (red){
+    console.log();
+    // On recupere l'élement sur lequel on veu ajouter le theme couleur
+    const mainElementRed =document.querySelector('main');
+    console.log(mainElementRed);
+
+    mainElementRed.classList.remove('theme-red');
+    console.log(mainElementRed);
+
+    mainElementRed.classList.add('red');
+    console.log(mainElementRed);
+ 
+},
+
+changeColorThemeBlue: function (blue){
+  console.log();
+  // On recupere l'élement sur lequel on veu ajouter le theme couleur
+  const mainElementBlue =document.querySelector('main');
+  console.log(mainElementBlue);
+
+  mainElementBlue.classList.remove('theme-blue');
+  console.log(mainElementBlue);
+
+  mainElementBlue.classList.add('blue');
+  console.log(mainElementBlue);
+
+},
+}
+document.addEventListener('DOMContentLoaded', theme.init);
